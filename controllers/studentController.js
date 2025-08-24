869e470d77007302f6b39c76df75bae5338b07a0
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-08-24 20:31:27 +0530

    adding data of student

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,11 +1,21 @@
 const db = require('../utils/db-connection');
 const studentModal = require('../models/student');
-const addStudentData = (async(req,res)=>{
-  const {name,email,age} = req.body;
-    const addStudentDataQuery = `INSERT INTO students (name,email,age) VALUES (?,?,?)`;
 
-  const response =  await studentModal.create({name,email,age})
-res.status(200).send(response);
+const addStudentData = (async (req, res) => {
+    try {
+        const { name, email, age } = req.body;
+        const response = await studentModal.create({
+            name: name,
+            email: email,
+            age: age
+        })
+        res.status(200).send(response);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+
+    // const addStudentDataQuery = `INSERT INTO students (name,email,age) VALUES (?,?,?)`;
     // db.execute(addStudentDataQuery,[name,email,age],(err)=>{
     //     if(err){
     //         res.status(500).send(err.message);
@@ -16,62 +26,114 @@ res.status(200).send(response);
     // })
 })
 
-const updateStudentData = ((req,res)=>{
-    const {id} = req.params;
-    const {name,email} = req.body;
-    const updateStudentDataQuery = `UPDATE students SET name=?,email=? WHERE id = ${id}`;
-    db.execute(updateStudentDataQuery,[name,email],(err,result)=>{
-        if(err){
-            res.status(500).send(err.message);
-            db.end();
-            return;
-        }
-        if(result.affectedRows ===0){
-            res.status(404).send("user not found");
-        }
-        res.status(200).send("student updated successfully");
-    })
+const updateStudentData = (async (req, res) => {
+
+    // try {
+    //     const { id } = req.params;
+    //     const { name, email } = req.body;
+    //     const student = await studentModal.findByPk(id);
+    //     if (!student) {
+    //         res.status(404).send("user not found");
+    //     }
+    //     student.name = name;
+    //     student.email = email;
+    //     await student.save();
+    //     res.status(200).send("user has been updated");
+
+    // } catch (er) {
+    //     res.status(500).send("user cannot be updated");
+    // }
+
+
+    // const updateStudentDataQuery = `UPDATE students SET name=?,email=? WHERE id = ${id}`;
+    // db.execute(updateStudentDataQuery, [name, email], (err, result) => {
+    //     if (err) {
+    //         res.status(500).send(err.message);
+    //         db.end();
+    //         return;
+    //     }
+    //     if (result.affectedRows === 0) {
+    //         res.status(404).send("user not found");
+    //     }
+    //     res.status(200).send("student updated successfully");
+    // })
 })
 
-const getStudentData = ((req,res)=>{
-    const getStudentDataQuery =`SELECT * FROM students`;
-    db.execute(getStudentDataQuery,(err,result)=>{
-        if(err){
-            res.status(500).send(err.message);
-            db.end();
-            return;
-        }
-        res.status(200).send(JSON.stringify(result));
-    })
+const getStudentData = (async (req, res) => {
+
+    // try {
+    //     const student = await studentModal.findAll();
+    //     if (!student) {
+    //         res.status(404).send("student not found");
+    //     }
+    //     res.status(200).json(student);
+    // } catch (err) {
+    //     res.status(500).send("error occurred while getting data");
+    // }
+    // const getStudentDataQuery = `SELECT * FROM students`;
+    // db.execute(getStudentDataQuery, (err, result) => {
+    //     if (err) {
+    //         res.status(500).send(err.message);
+    //         db.end();
+    //         return;
+    //     }
+    //     res.status(200).send(JSON.stringify(result));
+    // })
 })
 
-const getStudentDataById = ((req,res)=>{
-    const {id} = req.params;
-    const getStudentDataQuery =`SELECT * FROM students WHERE id = ${id}`;
-    db.execute(getStudentDataQuery,(err,result)=>{
-        if(err){
-            res.status(500).send(err.message);
-            db.end();
-            return;
-        }
-        res.status(200).send(JSON.stringify(result));
-    })
+const getStudentDataById = (async (req, res) => {
+
+    // try {
+    //     const { id } = req.params;
+    //     const student = await studentModal.findByPk(id);
+    //     if (!student) {
+    //         res.status(404).send("user not found");
+    //     }
+    //     res.status(200).json(student)
+    // } catch (err) {
+    //     res.status(500).send("error encountered while fetching");
+    // }
+
+    // const getStudentDataQuery = `SELECT * FROM students WHERE id = ${id}`;
+    // db.execute(getStudentDataQuery, (err, result) => {
+    //     if (err) {
+    //         res.status(500).send(err.message);
+    //         db.end();
+    //         return;
+    //     }
+    //     res.status(200).send(JSON.stringify(result));
+    // })
 })
 
-const deleteStudentDataById = ((req,res)=>{
-    const {id} = req.params;
-    const deleteStudentDataByIdQuery = `DELETE FROM students WHERE id = ${id}`;
-    db.execute(deleteStudentDataByIdQuery,(err,result)=>{
-        if(err){
-            res.status(500).send(err.message);
-            db.end();
-            return;
-        }
-        if(result.affectedRows===0){
-            res.status(404).send("student not found");
-        }
-        res.status(200).send('data deleted successfully');
-    })
+const deleteStudentDataById = (async (req, res) => {
+
+    // try {
+    //     const { id } = req.params;
+    //     const student = await studentModal.destroy({
+    //         where: {
+    //             id: id
+    //         }
+    //     })
+    //     if (!student) {
+    //         res.status(404).send("user is not found");
+    //     }
+    //     res.status(200).send("user is deleted");
+    // } catch (err) {
+    //     res.status(500).send("error encounter during deleting!");
+    // }
+
+    // const deleteStudentDataByIdQuery = `DELETE FROM students WHERE id = ${id}`;
+    // db.execute(deleteStudentDataByIdQuery, (err, result) => {
+    //     if (err) {
+    //         res.status(500).send(err.message);
+    //         db.end();
+    //         return;
+    //     }
+    //     if (result.affectedRows === 0) {
+    //         res.status(404).send("student not found");
+    //     }
+    //     res.status(200).send('data deleted successfully');
+    // })
 })
 
 
